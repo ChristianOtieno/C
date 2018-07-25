e7a02290f1e7a686ae01a4d1ff7fd848101f1304
Christian Otieno <cgao273@gmail.com>
2018-07-25 05:59:35 +0300

    node compiles ;-)

---
+++ node.c
@@ -0,0 +1,13 @@
+
+// A linked list node
+#include <stdio.h>
+int main(int argc, char const *argv[])
+{
+    printf("I am a node!");
+    struct Node
+{
+    int data;
+    struct Node*next;
+};
+    return 0;
+};
